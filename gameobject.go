dbbb89e8a8dba9d57b0bb8304e8dd1d8decf97e2
FLT4613 <ft4613st@gmail.com>
2021-05-18 07:07:25 +0900

    update

--- gameobject.go
+++ gameobject.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"log"
-
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/inpututil"
 )
@@ -54,7 +51,7 @@ func newGameObject(options ...Option) *GameObject {
 		option(&obj)
 	}
 	obj.MoveSpeed = 3
-	obj.acc.y = 0.05
+	// obj.acc.y = 0.005
 
 	obj.sm = newStateManager()
 
@@ -63,18 +60,13 @@ func newGameObject(options ...Option) *GameObject {
 		obj.walk()
 	}))
 	obj.sm.addState("JumpStart", onUpdate(func() {
-		obj.vec.y = -20
+		obj.vec.y = -9
 	}))
 	obj.sm.addState("AirIdle", onUpdate(func() {
-		if obj.acc.y == 0 {
-			obj.acc.y = 1
-		}
-
+		obj.acc.y = 0.4
 		obj.vec.y += obj.acc.y
-		log.Print(obj.acc.y)
 	}))
 	obj.sm.addState("Land", onUpdate(func() {
-		obj.acc.y = 0
 		obj.vec.y = 0
 		obj.isJumpReady = false
 	}))
@@ -137,76 +129,6 @@ func (obj *GameObject) walk() {
 
 func (obj *GameObject) update() {
 	obj.sm.update()
-	// if obj.pos.y < 400 {
-	obj.vec.Add(obj.acc)
-	// } else {
-	// 	obj.vec.y = 0
-	// 	obj.acc.y = 0
-	// }
+	// obj.vec.Add(obj.acc)
 	obj.pos.Add(obj.vec)
-}
-
-type StateManager struct {
-	states       map[string]State
-	currentState string
-	transitions  map[string][]func()
-}
-
-func newStateManager() *StateManager {
-	var s StateManager
-	s.states = map[string]State{}
-	s.currentState = ""
-	s.transitions = map[string][]func(){}
-	return &s
-}
-
-func (sm *StateManager) update() {
-	for _, transition := range sm.transitions[sm.currentState] {
-		transition()
-	}
-	sm.states[sm.currentState].onUpdate()
-}
-
-func (sm *StateManager) addState(newStateKey string, options ...StateOption) {
-	s := State{}
-	s.onEnter = func() {}
-	s.onUpdate = func() {}
-	s.onExit = func() {}
-	for _, option := range options {
-		option(&s)
-	}
-	sm.states[newStateKey] = s
-}
-
-func (sm *StateManager) addTransition(sources []string, destination string, condition func() bool) {
-	for _, source := range sources {
-		_, ok := sm.transitions[source]
-		if !ok {
-			sm.transitions[source] = []func(){}
-		}
-		sm.transitions[source] = append(sm.transitions[source], func() {
-			if condition() {
-				sm.changeState(destination)
-			}
-		})
-	}
-}
-
-func (sm *StateManager) changeState(key string) {
-	if sm.currentState == key {
-		return
-	}
-	if sm.currentState != "" {
-		current := sm.states[sm.currentState]
-		current.onExit()
-	}
-	log.Print(fmt.Sprintf("%v -> %v", sm.currentState, key))
-	sm.currentState = key
-	sm.states[sm.currentState].onEnter()
-}
-
-type State struct {
-	onEnter  func()
-	onUpdate func()
-	onExit   func()
 }
---
+++ state.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+type StateManager struct {
+	states       map[string]State
+	currentState string
+	transitions  map[string][]func()
+}
+
+func newStateManager() *StateManager {
+	var s StateManager
+	s.states = map[string]State{}
+	s.currentState = ""
+	s.transitions = map[string][]func(){}
+	return &s
+}
+
+func (sm *StateManager) update() {
+	for _, transition := range sm.transitions[sm.currentState] {
+		transition()
+	}
+	sm.states[sm.currentState].onUpdate()
+}
+
+func (sm *StateManager) addState(newStateKey string, options ...StateOption) {
+	s := State{}
+	s.onEnter = func() {}
+	s.onUpdate = func() {}
+	s.onExit = func() {}
+	for _, option := range options {
+		option(&s)
+	}
+	sm.states[newStateKey] = s
+}
+
+func (sm *StateManager) addTransition(sources []string, destination string, condition func() bool) {
+	for _, source := range sources {
+		_, ok := sm.transitions[source]
+		if !ok {
+			sm.transitions[source] = []func(){}
+		}
+		sm.transitions[source] = append(sm.transitions[source], func() {
+			if condition() {
+				sm.changeState(destination)
+			}
+		})
+	}
+}
+
+func (sm *StateManager) changeState(key string) {
+	if sm.currentState == key {
+		return
+	}
+	if sm.currentState != "" {
+		current := sm.states[sm.currentState]
+		current.onExit()
+	}
+	log.Print(fmt.Sprintf("%v -> %v", sm.currentState, key))
+	sm.currentState = key
+	sm.states[sm.currentState].onEnter()
+}
+
+type State struct {
+	onEnter  func()
+	onUpdate func()
+	onExit   func()
+}
